4c63fde7694a4d2879c553aa0dd1164773958ced
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-07 18:23:02 -0300

    Some Maps

--- src/load_game.c
+++ src/load_game.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 22:19:02 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/07 17:28:36 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/07 18:12:54 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	load_game(t_game *game)
 {
-	//game->moves = 10;
 	game->mlx = mlx_init();
-	printf("Passou");
+	game->win = mlx_new_window(game->mlx, 500, 500, "So_Long");
+	printf("\nPassou\n");
 }
