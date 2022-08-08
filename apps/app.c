a5ca0c272b00ca2f2854d62386856d9f91bdc32f
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-08 08:15:00 -0300

    Samples of code

--- apps/app.c
+++ apps/app.c
@@ -6,13 +6,44 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 18:55:06 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/04 20:42:50 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/08 08:12:58 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include "../include/so_long.h"
+
+// int	main(int argc, char **argv) 
+// {
+// 	start(argc, argv);
+// }
+
+
+// *--* INICIO harm-smits INICIO  *--*
+
+// // *--* INICIO Exemplo 1 - Initialize MiniLibX
+// #include "../include/so_long.h"
+
+// int	main(void)
+// {
+// 	void	*mlx;
+
+// 	mlx = mlx_init();
+// 	printf("oi oi io \n");
+// }
+// // *--* FIM Exemplo 1 - Initialize MiniLibX
+
+// *--* INICIO Exemplo 2 - Open Window
 #include "../include/so_long.h"
 
-int	main(int argc, char **argv) 
+int	main(void)
 {
-	start(argc, argv);
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
+// *--* FIM Exemplo 2 - Open Window
+
+// *--* FIM harm-smits INICIO  *--*
