493baf2b78bd36d4907f08c283e25ffc42099503
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-19 13:29:42 -0300

    refactoring and update documentation

--- src/draw_imgs.c
+++ src/draw_imgs.c
@@ -6,40 +6,62 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 16:20:42 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/18 08:00:24 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/19 08:12:04 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void	put_img(t_game *game, void *img, int x, int y) // TODO: Teria q ser estático?
+/**
+ * @brief Responsable to put a image in window. All the 
+ * chars will use this call 
+ * @param game 
+ * @param img image setup previus
+ * @param x coordinates define where the image should be placed in the window.
+ * @param y coordinates define where the image should be placed in the window.
+ */
+static	void put_img(t_game *game, void *img, int x, int y)
 {
 	mlx_put_image_to_window(game->mlx, game->win, img, x * IMG_PXL_SZ, y * IMG_PXL_SZ);
 }
 
-
-static	void setup_exit_img(t_game *game, void *img, int x, int y)
+/**
+ * @brief Set the up exit img object.
+ * If This will be the last image (exit change) to show that player now must
+ * exit the game 
+ * @param game 
+ * @param x coordinates define where the image should be placed in the window.
+ * @param y coordinates define where the image should be placed in the window.
+ */
+static	void setup_exit_img(t_game *game, int x, int y)
 {
-	// Just finish if all collectibles was collected
+	// If all collectible was done
 	if (game->n_collectible == 0)
 	{
+		// Remove img actual
 		mlx_destroy_image(game->mlx, game->img_exit);
+		// Change img to new exit
 		game->img_exit = mlx_xpm_file_to_image(
 			game->mlx, "img/e-2.xpm", &game->img_w, &game->img_h);
-
 	}
 	put_img(game, game->img_exit, x, y);
 }
 
-
-static	void setup_player_img(t_game *game, void *img, int x, int y)
+/**
+ * @brief Set the up player img object.
+ * Update the user position (x_player and y_player)
+ * @param game 
+ * @param x coordinates define where the image should be placed in the window.
+ * @param y coordinates define where the image should be placed in the window.
+ */
+static	void setup_player_img(t_game *game, int x, int y)
 {
 	game->x_player = x;
 	game->y_player = y;
 	put_img(game, game->img_player, x, y);
 }
 
-void	map_draw_img(t_game *game)
+int	map_draw_img(t_game *game)
 {
 	int	x;
 	int y;
@@ -48,6 +70,7 @@ void	map_draw_img(t_game *game)
 	while (game->map[y])
 	{
 		x = 0;
+		// Loop between all the chars
 		while (game->map[y][x])
 		{
 			if (game->map[y][x] == '1')
@@ -55,13 +78,14 @@ void	map_draw_img(t_game *game)
 			else if (game->map[y][x] == '0')
 				put_img(game, game->img_empty, x, y);
 			else if (game->map[y][x] == 'P')
-				setup_player_img(game, game->img_player, x, y);
+				setup_player_img(game, x, y);
 			else if (game->map[y][x] == 'C')
 				put_img(game, game->img_collectible, x, y);
 			else if (game->map[y][x] == 'E')
-				setup_exit_img(game, game->img_exit, x, y);
+				setup_exit_img(game, x, y);
 			x++;
 		}
 		y++;
 	}
+	return(0);
 }
