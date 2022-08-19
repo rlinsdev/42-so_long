4e14dba2233923a2aa66a28549f0fbcaa8c2799f
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-19 13:29:05 -0300

    Refactoring and Exit game

--- src/key_events.c
+++ src/key_events.c
@@ -6,54 +6,58 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 08:06:03 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/18 08:32:00 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/19 13:28:03 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+/**
+ * @brief Responsable to call a correct press buton method to take action
+ * @param keycode KeyCode of keybord pressed
+ * @param game Game object
+ */
 static	void handle_keys(int keycode, t_game *game)
 {
 	if (keycode == XK_w || keycode == XK_Up)
 	{
 		ft_printf("\nw apertado - %d\n", keycode);
-		game->y_player -= 1;
 		press_w(game);
-		// game->moves++;
 	}
 	else if (keycode == XK_a || keycode == XK_Left)
 	{
 		ft_printf("\na apertado - %d\n", keycode);
-		game->x_player -= 1;
 		press_a(game);
-		// game->moves++;
 	}
 	else if (keycode == XK_s || keycode == XK_Down)
 	{
 		ft_printf("\ns apertado - %d\n", keycode);
-		game->y_player += 1;
 		press_s(game);
-		// game->moves++;
 	}
 	else if (keycode == XK_d || keycode == XK_Right)
 	{
 		ft_printf("\nd apertado - %d\n", keycode);
-		game->x_player += 1;
 		press_d(game);
-		// game->moves++;
 	}
 }
 
+/**
+ * @brief Called by Hook key press.
+ * Any key pressed in window, will call this function 
+ * @param keycode 
+ * @param game 
+ * @return int. Important to MLX framework return a int
+ */
 int	key_press(int keycode, t_game *game)
 {
-	ft_printf("\nEntrou no Key Press");
-	//if (keycode == KEY_ESC || keycode == KEY_Q)
+	// Exit game when esc
 	if (keycode == XK_Escape)
 		exit_game(game);
-	else 
+	else if (!game->endgame)
 	{
 		handle_keys(keycode, game);
 		ft_printf("Number of Movements: %d", game->moves);
 	}
+	// When endgame==1, just stop to take actions
 	return (0);
 }
