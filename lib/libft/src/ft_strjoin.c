48494094c77852be7a5d246edcb5bbc13b4219c5
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-19 13:51:45 -0300

    removing comment code

--- lib/libft/src/ft_strjoin.c
+++ lib/libft/src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 14:42:50 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/13 14:45:59 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/19 13:51:04 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,4 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		*str++ = *s2++;
 	*str = '\0';
 	return (mark);
-	// char	*result;
-	// int		size;
-
-	// size = ft_strlen(s1) + ft_strlen(s2) + 1;
-	// result = malloc(size * sizeof(char));
-	// if (result == NULL)
-	// 	return (NULL);
-	// ft_strlcpy(result, s1, ft_strlen(s1) + 1);
-	// ft_strlcat(result, s2, size);
-	// return (result);
 }
