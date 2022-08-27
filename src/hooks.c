e9d9459589fc36222078f4a275a14ddf7945bb74
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-27 11:30:01 -0300

    Chaging Hook to Loop Hook to map draw img

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 08:10:37 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/20 17:17:10 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/27 11:29:52 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ void	hooks_handler(t_game *game)
 	mlx_hook(game->win, KeyPress, KeyPressMask, key_press, game);
 	mlx_hook(game->win, DestroyNotify, StructureNotifyMask,
 		exit_game, game);
-	mlx_hook(game->win, FocusIn, FocusChangeMask, map_draw_img,
-		game);
+	mlx_loop_hook(game->mlx, map_draw_img, game);
 }
