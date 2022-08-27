eb093ed882f8928460ecb84e6ddfc0b57040418f
Rodrigo Lins <rlinsdev@gmail.com>
2022-08-27 07:41:34 -0300

    Fixing App.c - Norminette

--- apps/app.c
+++ apps/app.c
@@ -6,13 +6,13 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 18:55:06 by rlins             #+#    #+#             */
-/*   Updated: 2022/08/16 08:33:20 by rlins            ###   ########.fr       */
+/*   Updated: 2022/08/27 07:41:12 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "../include/so_long.h"
+#include "../include/so_long.h"
 
-int	main(int argc, char **argv) 
+int	main(int argc, char **argv)
 {
 	start(argc, argv);
 	return (0);
